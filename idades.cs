27b1a5a15e5fdcb5dc61a200a705c7b9dfc0e27e
Clara-Noah <claraclaudio806@gmail.com>
2024-11-15 13:06:20 +0000

    Add files via upload

---
+++ idades.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+public class Program
+    {
+    static void Main(string[] args)
+    {
+        //Cria um algoritemo que descubra se o utilizador é ma criança, adolescente ou adulto
+        int idade = 19;
+        bool isSolteiro = false;
+
+        if (idade < 18)
+        {
+            Console.WriteLine("És menor de idade");
+        }
+        else if (idade >= 18 && idade < 65 && isSolteiro)
+        {
+           Console.WriteLine("É adulto e solteiro");
+        }
+        else if (idade >= 18 && idade < 65 && !isSolteiro)
+        { 
+         Console.WriteLine("É adulto e casado");
+        }
+        else
+        {
+            Console.WriteLine("És sénior");
+        }
+
+      }
+    }
+
